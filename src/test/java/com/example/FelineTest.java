69177e1f6b107768bc084009bc38ad2a6aadf14b
Spitsulya <spitsulya15@yandex.ru>
2025-01-15 22:01:45 +0300

    Added tests for Feline class in FelineTest

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,7 +1,48 @@
 package com.example;
-
-import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
 
 public class FelineTest {
 
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+    Animal animal = Mockito.mock(Animal.class);
+    Feline feline = new Feline();
+
+    @Test
+    public void testEatMeat() throws Exception {
+
+        List<String> expectedFood = Arrays.asList("Животные", "Птицы", "Рыба");
+        Mockito.when(animal.getFood("Хищник")).thenReturn(expectedFood);
+
+        // Проверяем, что метод eatMeat возвращает ожидаемый результат
+        List<String> actualFood = feline.eatMeat();
+        assertEquals(expectedFood, actualFood);
+    }
+
+    @Test
+    // Проверяем, что метод getFamily возвращает "Кошачьи"
+    public void testGetFamily() {
+        assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    // Проверяем, что метод getKittens без параметров возвращает 1
+    @Test
+    public void testGetKittens() {
+        assertEquals(1, feline.getKittens());
+    }
+
+    // Проверяем, что метод getKittens с параметром возвращает переданное значение
+    @Test
+    public void testGetKittensWithParameter() {
+        int kittensCount = 5;
+        assertEquals(kittensCount, feline.getKittens(kittensCount));
+    }
 }
