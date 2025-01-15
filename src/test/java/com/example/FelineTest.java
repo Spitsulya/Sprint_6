af021d95c2333f2b6db365040bb0647b6ce51122
Spitsulya <spitsulya15@yandex.ru>
2025-01-15 22:16:50 +0300

    Added parameterized parameters for Feline.testGetKittensWithParameter method

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,12 +1,15 @@
 package com.example;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import java.util.Arrays;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class FelineTest {
 
     @Before
@@ -16,19 +19,20 @@ public class FelineTest {
     Animal animal = Mockito.mock(Animal.class);
     Feline feline = new Feline();
 
+    // Проверяем, что метод eatMeat возвращает ожидаемый результат
     @Test
     public void testEatMeat() throws Exception {
 
         List<String> expectedFood = Arrays.asList("Животные", "Птицы", "Рыба");
         Mockito.when(animal.getFood("Хищник")).thenReturn(expectedFood);
 
-        // Проверяем, что метод eatMeat возвращает ожидаемый результат
         List<String> actualFood = feline.eatMeat();
         assertEquals(expectedFood, actualFood);
     }
 
-    @Test
     // Проверяем, что метод getFamily возвращает "Кошачьи"
+    @Test
+
     public void testGetFamily() {
         assertEquals("Кошачьи", feline.getFamily());
     }
@@ -39,10 +43,25 @@ public class FelineTest {
         assertEquals(1, feline.getKittens());
     }
 
+    // Параметры для теста getKittens
+    @Parameterized.Parameter(0)
+    public int inputKittensCount;
+
+    @Parameterized.Parameter(1)
+    public int expectedKittensCount;
+
+    @Parameterized.Parameters
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {1, 1},  // Тест для 1 котенка
+                {5, 5},  // Тест для 5 котят
+                {10, 10} // Тест для 10 котят
+        });
+    }
+
     // Проверяем, что метод getKittens с параметром возвращает переданное значение
     @Test
     public void testGetKittensWithParameter() {
-        int kittensCount = 5;
-        assertEquals(kittensCount, feline.getKittens(kittensCount));
+        assertEquals(expectedKittensCount, feline.getKittens(inputKittensCount));
     }
 }
